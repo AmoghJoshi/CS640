881336ef366de7153e1f3ae7a34285a5f8cd9fb8
GUG11 <hyzhang2112@gmail.com>
2017-10-04 00:21:54 -0500

    Fix LRU bugs

--- p1/myswitch_lru.py
+++ p1/myswitch_lru.py
@@ -13,22 +13,27 @@ class ForwardTable(object):
     CAPACITY=5
 
     def __init__(self):
-        self._table = dict()   # (key, (intf, timeout))
+        self._table = dict()   # (key, [intf, timeout])
+        self._count = 0
 
-    def insert(self, ethaddr, intf_name, timestamp):
+    def insert(self, ethaddr, intf_name):
         if ethaddr in self._table:
-            self._table[ethaddr][0] = intf_name
-            log_debug("FWT: modify entry ({},({},{}))".format(ethaddr, intf_name, timestamp))
+            self._table[ethaddr] = (intf_name, self._table[ethaddr][1])
+            log_debug("FWT: modify entry ({},{}) -> ({},{}))"
+                    .format(ethaddr, self._table[ethaddr][0], ethaddr, intf_name))
         else:
             if len(self._table) == ForwardTable.CAPACITY:
                 entry_to_remove = min(self._table.items(), key=lambda entry : entry[1][1])
                 self._table.pop(entry_to_remove[0])
                 log_debug("FWT: table size reaches {}, pop entry {}".format(ForwardTable.CAPACITY, entry_to_remove))
-            self._table[ethaddr] = (intf_name, timestamp)
-            log_debug("FWT: insert entry ({},({},{}))".format(ethaddr, intf_name, timestamp))
+            self._table[ethaddr] = (intf_name, self._count)
+            log_debug("FWT: insert entry ({},({},{}))".format(ethaddr, intf_name, self._count))
+            self._count += 1
 
     def lookup(self, ethaddr):
         if ethaddr in self._table:
+            self._table[ethaddr] = (self._table[ethaddr][0], self._count)
+            self._count += 1
             return self._table[ethaddr][0]
         else:
             return None
@@ -42,7 +47,7 @@ def main(net):
     while True:
         try:
             timestamp,input_port,packet = net.recv_packet()
-            forward_table.insert(packet[0].src, input_port, timestamp);
+            forward_table.insert(packet[0].src, input_port);
         except NoPackets:
             continue
         except Shutdown:
