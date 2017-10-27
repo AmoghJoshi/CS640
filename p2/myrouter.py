4f903dc11f5259b1a65e4e7266e9f2f322101702
GUG11 <hyzhang2112@gmail.com>
2017-10-27 11:39:51 -0500

    multithreading

--- p2/myrouter.py
+++ p2/myrouter.py
@@ -7,7 +7,27 @@ Basic IPv4 router (static routing) in Python.
 import sys
 import os
 import time
+import threading
 from switchyard.lib.userlib import *
+
+next_tid = 0
+
+class ArpMapping(object):
+    def __init__(self):
+        self.ip2eth_lock = threading.Lock()
+        self.ip2eth = dict()
+
+    def put(self, arp):
+        self.ip2eth_lock.acquire()
+        self.ip2eth[arp.senderprotoaddr] = arp.senderhwaddr
+        self.ip2eth_lock.release()
+
+    def contains(self, ipv4_addr):
+        return ipv4_addr in self.ip2eth
+
+    def get(self, ipv4_addr):
+        return self.ip2eth[ipv4_addr]
+
 
 class ForwardTable(object):
     def __init__(self, path):
@@ -29,6 +49,46 @@ class ForwardTable(object):
         return matching_entry
 
 
+class BackgroundForwarding(threading.Thread):
+    def __init__(self, tid, net, arp_mapping, intf, next_hop, pending_pkt):
+        threading.Thread.__init__(self)
+        self.net = net
+        self._tid = tid
+        self._arp_mapping = arp_mapping
+        self._intf = intf
+        self._next_hop = next_hop
+        self._pending_pkt = pending_pkt
+
+    def run(self):
+        log_debug('start thread {}'.format(self._tid))
+        if self._arp_mapping.contains(self._next_hop):
+            self.broadcast_arp_request(self._intf, self._next_hop)
+        pkt[Ethernet].src = self._intf.ethaddr
+        pkt[Ethernet].dst = self._arp_mapping.get(self._next_hop)
+        self.net.send_packet(self._intf.name, self._pending_pkt)
+
+    def broadcast_arp_request(self, intf, target_ip_addr):
+        arp_req = create_ip_arp_request(intf.ethaddr, intf.ipaddr, target_ip_addr)
+        self.net.send_packet(intf.name, arp_req)
+        count = 0
+        gotpkt = False
+        while count < 5 and not gotpkt:
+            try:
+                timestamp,dev,pkt = self.net.recv_packet(timeout=1.0)
+                if pkt.has_header(Arp):
+                    arp_hdr = pkt.get_header(Arp)
+                    if arp_hdr.operation = Arp.Reply and arp_hdr.senderprotoaddr == target_ip_addr:
+                        gotpkt = True
+            except NoPackets:
+                log_debug("No packets available in recv_packet. {}th attempt.".format(count+1))
+            except Shutdown:
+                log_debug("Got shutdown signal")
+                return
+            finally:
+                count += 1
+        self.process_arp(pkt, dev)
+
+
 class Router(object):
     def __init__(self, net):
         self.net = net
@@ -36,14 +96,15 @@ class Router(object):
         # other initialization stuff here
         self.ethaddrs = set([intf.ethaddr for intf in self.my_interfaces])
         self.ipaddrs = set([intf.ipaddr for intf in self.my_interfaces])
-        self.ip2eth = dict()
+        self.arp_mapping = ArpMapping()
+        self.bg_threads = []
         if os.path.exists('forwarding_table.txt'):
             self.fwd_table = ForwardTable('forwarding_table.txt')
 
     def process_arp(self, pkt, input_port):
         arp = pkt.get_header(Arp)
         # store the mapping, applicable for both REQUEST and REPLY
-        self.ip2eth[arp.senderprotoaddr] = arp.senderhwaddr
+        self.arp_mapping.put(arp)
         # REQUEST
         if arp.operation == ArpOperation.Request:
             if arp.targetprotoaddr in self.ipaddrs:
@@ -53,41 +114,22 @@ class Router(object):
                         arp.senderhwaddr, arp.targetprotoaddr, arp.senderprotoaddr)
                 self.net.send_packet(input_port, arp_reply)
 
-    def broadcast_arp_request(self, eth_port, target_ip_addr):
-        arp_req = create_ip_arp_request(intf.ethaddr, intf.ipaddr, target_ip_addr)
-        self.net.send_packet(eth_port, arp_req)
-        count = 0
-        while count < 5 and not gotpkt:
-            try:
-                # TODO: get out of blocked
-                timestamp,dev,pkt = self.net.recv_packet(timeout=1.0)
-            except NoPackets:
-                log_debug("No packets available in recv_packet. {}th attempt.".format(count+1))
-                gotpkt = False
-            else:
-                gotpkt = True
-            except Shutdown:
-                log_debug("Got shutdown signal")
-                return
-            finally:
-                count += 1
-        self.process_arp(pkt, dev)
-
     def process_ipv4(self, pkt, input_port):
         pkt.ttl = pkt.ttl - 1
         ip_hdr = pkt.get_header(IPv4)
         if ip_hdr.dst in self.ipaddrs:
             return # drop the packet intended for the router
-        match_entry = self.fwd_table.lookup(ip_hdr.dst)
-        if match_entry is None:
+        self.match_entry = self.fwd_table.lookup(ip_hdr.dst)
+        if self.match_entry is None:
             return # drop if mismatch
         # forwarding
         next_hop, eth_port, _ = match_entry
-        if next_hop not in self.ip2eth:
-            self.broadcast_arp_request(eth_port, next_hop)
-        pkt[Ethernet].src = self.net.interface_by_name(eth_port).ethaddr
-        pkt[Ethernet].dst = self.ip2eth[next_hop]
-        self.net.send_packet(eth_port, pkt)
+        intf = self.net.interface_by_name(eth_port)
+        self.bg_threads.append(BackgroundForwarding(next_tid, self.net, self.arp_mapping, intf, next_hop, pkt))
+        next_tid += 1
+        self.bg_threads[-1].start()
+
+
 
     def process_packet(self, pkt, input_port):
         if pkt.has_header(Arp):
@@ -96,7 +138,6 @@ class Router(object):
         elif pkt.has_header(IPv4):
             log_debug("{} has an IPv4 header".format(pkt))
             self.process_ipv4(pkt, input_port)
-
 
     def router_main(self):
         '''
